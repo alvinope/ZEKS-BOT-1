4bdd3a377afa48954aa6b079dfc6c1bf05887bfb
ZEKS-TEAM <74397872+zeksteam@users.noreply.github.com>
2020-11-13 14:32:07 +0700

    UPDATE 2.0 [BETA]

---
+++ README.md
@@ -0,0 +1,150 @@
+SC ORIGINAL ARUGAZ, RECODE BY VINZGAMING
+
+## Getting Started
+
+This project require NodeJS v12.
+
+### Install
+Clone this project
+
+```bash
+> git clone https://github.com/ArugaZ/whatsapp-bot.git
+> cd whatsapp-bot
+```
+
+Install the dependencies:
+
+```bash
+> npm install 
+> npm install gify-cli -g
+```
+
+### Usage
+Run the Whatsapp bot
+
+```bash
+> npm start
+```
+
+after running it you need to scan the qr
+
+### Information
+- Change ownerNumber on settings/setting.json
+- Change groupLimit on settings/setting.json
+- Change memberLimit on settings/setting.json
+- Change prefix on settings/setting.json
+- Change all apiKey settings/api.json
+- Change menu on lib/menu.js
+pls dont remove my github link:)
+
+---
+
+## Features
+| Creator |Yes|
+| ------------- | ------------- |
+| Respond img to sticker|✅|
+| Respond img to sticker no bg|✅|
+| Respond url to sticker|✅|
+| Respond gif to sticker|✅|
+| Respond giphy url to sticker|✅|
+| Make a meme from photo|✅|
+| Quotes maker result pict|✅|
+| Nulis Bot|✅|
+| Toimg|✅|
+| QRcode |✅|
+
+| Islam |Yes|
+| ------------- | ------------- |
+| List Surah|✅|
+| Info Surah|✅|
+| Surah|✅|
+| Tafsir Alquran|✅|
+| Alquran Audio/Voice|✅|
+| Jadwal solat|✅|
+
+| Downloader |Yes|
+| ------------- | ------------- |
+| Instagram |✅|
+| Youtube Music |✅|
+| Youtube Video |✅|
+
+
+| Kerang Ajaib|Yes|
+
+| Searchs |Yes|
+| ------------- | ------------- |
+| Images |✅|
+| Subreddit |✅|
+| Resep makanan |✅|
+| Stalk IG |✅|
+| Wikipedia |✅|
+| Cuaca |✅|
+| Chord music |✅|
+| Screen Crot!|✅|
+| Play music|✅|
+| whats anime?|✅|
+| Covidindo|✅|
+
+| Random text |Yes|
+| ------------- | ------------- |
+| Pantun pakboy|✅|
+| Fakta Menarik|✅|
+| Kata Bijak|✅|
+| Quotes|✅|
+
+| Random image |Yes|
+| ------------- | ------------- |
+| Anime |✅|
+| Kpop |✅|
+| Memes |✅|
+
+
+| Others |Yes|
+| ------------- | ------------- |
+| Teks to Sound/Voice|✅|
+| Translate teks|✅|
+| Get covid info from map|✅|
+| Shortlink|✅|
+| Groupinfo|✅|
+
+| Groups |Yes|
+| ------------- | ------------- |
+| Admin||
+| Add user|✅|
+| Kick user|✅|
+| Promote User|✅|
+| Demote User|✅|
+| Delete bot msg|✅|
+| Tagall/mentions all|✅|
+| Owner||
+| Kick all members|✅|
+
+| Owner bot |Yes|
+| ------------- | ------------- |
+| Broadcast|✅|
+| Leave all group|✅|
+| Delete all msgs|✅|
+| Banned user|✅|
+
+
+## To-Do
+ - Add Media Downloader
+ - Add More Feature
+ - More refactoring
+ 
+---
+
+## Troubleshooting
+Make sure all the necessary dependencies are installed: https://github.com/puppeteer/puppeteer/blob/main/docs/troubleshooting.md
+
+Fix Stuck on linux, install google chrome stable: 
+```bash
+> wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+> sudo apt install ./google-chrome-stable_current_amd64.deb
+```
+
+## Thanks to
+- [WA-Automate](https://github.com/open-wa/wa-automate-nodejs)
+- [YogaSakti](https://github.com/YogaSakti/imageToSticker)
+- [MhankBarBar](https://github.com/MhankBarBar/whatsapp-bot)
+- [ArugaZ](https://github.com/ArugaZ)
---
+++ index.js
@@ -0,0 +1,88 @@
+const { create, Client } = require('@open-wa/wa-automate')
+const figlet = require('figlet')
+const options = require('./utils/options')
+const { color, messageLog } = require('./utils')
+const HandleMsg = require('./HandleMsg')
+
+const start = (aruga = new Client()) => {
+    console.log(color(figlet.textSync('----------------', { horizontalLayout: 'default' })))
+    console.log(color(figlet.textSync('ZEKS TEAM', { font: 'Ghost', horizontalLayout: 'default' })))
+    console.log(color(figlet.textSync('----------------', { horizontalLayout: 'default' })))
+    console.log(color('[DEV]'), color('VinZ', 'yellow'))
+    console.log(color('[~>>]'), color('BOT Started!', 'green'))
+
+    // Mempertahankan sesi agar tetap nyala
+    aruga.onStateChanged((state) => {
+        console.log(color('[~>>]', 'red'), state)
+        if (state === 'CONFLICT' || state === 'UNLAUNCHED') aruga.forceRefocus()
+    })
+
+    // ketika bot diinvite ke dalam group
+    aruga.onAddedToGroup(async (chat) => {
+	const groups = await aruga.getAllGroups()
+	// kondisi ketika batas group bot telah tercapai,ubah di file settings/setting.json
+	if (groups.length > groupLimit) {
+	await aruga.sendText(chat.id, `Sorry, Sekarang Pay 5k/bulan`).then(() => {
+	      aruga.leaveGroup(chat.id)
+	      aruga.deleteChat(chat.id)
+	  }) 
+	} else {
+	// kondisi ketika batas member group belum tercapai, ubah di file settings/setting.json
+	    if (chat.groupMetadata.participants.length < memberLimit) {
+	    await aruga.sendText(chat.id, `Sorry,Member Nya Kurang dari 10`).then(() => {
+	      aruga.leaveGroup(chat.id)
+	      aruga.deleteChat(chat.id)
+	    })
+	    } else {
+        await aruga.simulateTyping(chat.id, true).then(async () => {
+          await aruga.sendText(chat.id, `Hai minna~, Im Aruga BOT. To find out the commands on this bot type ${prefix}menu`)
+        })
+	    }
+	}
+    })
+
+    // ketika seseorang masuk/keluar dari group
+    aruga.onGlobalParicipantsChanged(async (event) => {
+        const host = await aruga.getHostNumber() + '@c.us'
+        // kondisi ketika seseorang diinvite/join group lewat link
+        if (event.action === 'add' && event.who !== host) {
+            await aruga.sendTextWithMentions(event.chat, `ALLO KAK @${event.who.replace('@c.us', '')} \n\nBeliin Seblak Buat Member Yak AWOKAWOK *[ZEKSBOT]*`)
+        }
+        // kondisi ketika seseorang dikick/keluar dari group
+        if (event.action === 'remove' && event.who !== host) {
+            await aruga.sendTextWithMentions(event.chat, `TELAH KELUAR @${event.who.replace('@c.us', '')}, Semoga Tenang Amin AWOKAWOK *[ZEKSBOT]*`)
+        }
+    })
+
+    aruga.onIncomingCall(async (callData) => {
+        // ketika seseorang menelpon nomor bot akan mengirim pesan
+        await aruga.sendText(callData.peerJid, 'Maaf VC/TELP AUTO BLOK.')
+        .then(async () => {
+            // bot akan memblock nomor itu
+            await aruga.contactBlock(callData.peerJid)
+        })
+    })
+
+    // ketika seseorang mengirim pesan
+    aruga.onMessage(async (message) => {
+        aruga.getAmountOfLoadedMessages() // menghapus pesan cache jika sudah 3000 pesan.
+            .then((msg) => {
+                if (msg >= 3000) {
+                    console.log('[aruga]', color(`Bused Pesan Gw ${msg}, Hapus Dlu Yekan`, 'yellow'))
+                    aruga.cutMsgCache()
+                }
+            })
+        HandleMsg(aruga, message)    
+    
+    })
+	
+    // Message log for analytic
+    aruga.onAnyMessage((anal) => { 
+        messageLog(anal.fromMe, anal.type)
+    })
+}
+
+//create session
+create(options(true, start))
+    .then((aruga) => start(aruga))
+    .catch((err) => new Error(err))
