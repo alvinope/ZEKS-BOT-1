1148efc4dc3f9fbc4a76ced1583bc18c05f37fed
ZEKS-TEAM <74397872+zeksteam@users.noreply.github.com>
2020-12-13 13:07:18 +0700

    new client

--- index.js
+++ index.js
@@ -4,78 +4,78 @@ const options = require('./utils/options')
 const { color, messageLog } = require('./utils')
 const HandleMsg = require('./HandleMsg')
 
-const start = (aruga = new Client()) => {
+const start = (vinz = new Client()) => {
     console.log(color(figlet.textSync('----------------', { horizontalLayout: 'default' })))
-    console.log(color(figlet.textSync('ZEKS TEAM', { font: 'Ghost', horizontalLayout: 'default' })))
+    console.log(color(figlet.textSync('ZEKS BOT', { font: 'Ghost', horizontalLayout: 'default' })))
     console.log(color(figlet.textSync('----------------', { horizontalLayout: 'default' })))
     console.log(color('[DEV]'), color('VinZ', 'yellow'))
     console.log(color('[~>>]'), color('BOT Aktif! Jangan Lupa Istirahat :D', 'green'))
 
     // Mempertahankan sesi agar tetap nyala
-    aruga.onStateChanged((state) => {
+    vinz.onStateChanged((state) => {
         console.log(color('[~>>]', 'red'), state)
-        if (state === 'CONFLICT' || state === 'UNLAUNCHED') aruga.forceRefocus()
+        if (state === 'CONFLICT' || state === 'UNLAUNCHED') vinz.forceRefocus()
     })
 
     // ketika bot diinvite ke dalam group
-    aruga.onAddedToGroup(async (chat) => {
-	const groups = await aruga.getAllGroups()
+    vinz.onAddedToGroup(async (chat) => {
+	const groups = await vinz.getAllGroups()
 	// kondisi ketika batas group bot telah tercapai,ubah di file settings/setting.json
 	if (groups.length > groupLimit) {
-	await aruga.sendText(chat.id, `Sorry, Sekarang Pay 5k/bulan`).then(() => {
-	      aruga.leaveGroup(chat.id)
-	      aruga.deleteChat(chat.id)
+	await vinz.sendText(chat.id, `Sorry, Sekarang Pay 5k/bulan`).then(() => {
+	      vinz.leaveGroup(chat.id)
+	      vinz.deleteChat(chat.id)
 	  }) 
 	} else {
-        await aruga.simulateTyping(chat.id, true).then(async () => {
-          await aruga.sendText(chat.id, `Hai minna~, Im ZEKS BOT. To find out the commands on this bot type ${prefix}menu`)
+        await vinz.simulateTyping(chat.id, true).then(async () => {
+          await vinz.sendText(chat.id, `Hai minna~, Im ZEKS BOT. To find out the commands on this bot type ${prefix}menu`)
         })
 	    }
 	}
     })
 
     // ketika seseorang masuk/keluar dari group
-    aruga.onGlobalParicipantsChanged(async (event) => {
-        const host = await aruga.getHostNumber() + '@c.us'
+    vinz.onGlobalParicipantsChanged(async (event) => {
+        const host = await vinz.getHostNumber() + '@c.us'
         // kondisi ketika seseorang diinvite/join group lewat link
         if (event.action === 'add' && event.who !== host) {
-            await aruga.sendTextWithMentions(event.chat, `ALLO KAK @${event.who.replace('@c.us', '')} \n\nBeliin Seblak Buat Member Yak AWOKAWOK *[ZEKSBOT]*`)
+            await vinz.sendTextWithMentions(event.chat, `ALLO KAK @${event.who.replace('@c.us', '')} \n\nBeliin Seblak Buat Member Yak AWOKAWOK *[ZEKSBOT]*`)
         }
         // kondisi ketika seseorang dikick/keluar dari group
         if (event.action === 'remove' && event.who !== host) {
-            await aruga.sendTextWithMentions(event.chat, `TELAH KELUAR @${event.who.replace('@c.us', '')}, Semoga Tenang Amin AWOKAWOK *[ZEKSBOT]*`)
+            await vinz.sendTextWithMentions(event.chat, `TELAH KELUAR @${event.who.replace('@c.us', '')}, Semoga Tenang Amin AWOKAWOK *[ZEKSBOT]*`)
         }
     })
 
-    aruga.onIncomingCall(async (callData) => {
+    vinz.onIncomingCall(async (callData) => {
         // ketika seseorang menelpon nomor bot akan mengirim pesan
-        await aruga.sendText(callData.peerJid, 'Maaf VC/TELP AUTO BLOK.')
+        await vinz.sendText(callData.peerJid, 'Maaf VC/TELP AUTO BLOK.')
         .then(async () => {
             // bot akan memblock nomor itu
-            await aruga.contactBlock(callData.peerJid)
+            await vinz.contactBlock(callData.peerJid)
         })
     })
 
     // ketika seseorang mengirim pesan
-    aruga.onMessage(async (message) => {
-        aruga.getAmountOfLoadedMessages() // menghapus pesan cache jika sudah 3000 pesan.
+    vinz.onMessage(async (message) => {
+        vinz.getAmountOfLoadedMessages() // menghapus pesan cache jika sudah 3000 pesan.
             .then((msg) => {
                 if (msg >= 3000) {
-                    console.log('[aruga]', color(`Bused Pesan Gw ${msg}, Hapus Dlu Yekan`, 'yellow'))
-                    aruga.cutMsgCache()
+                    console.log('[vinz]', color(`Bused Pesan Gw ${msg}, Hapus Dlu Yekan`, 'yellow'))
+                    vinz.cutMsgCache()
                 }
             })
-        HandleMsg(aruga, message)    
+        HandleMsg(vinz, message)    
     
     })
 	
     // Message log for analytic
-    aruga.onAnyMessage((anal) => { 
+    vinz.onAnyMessage((anal) => { 
         messageLog(anal.fromMe, anal.type)
     })
 }
 
 //create session
 create(options(true, start))
-    .then((aruga) => start(aruga))
+    .then((vinz) => start(vinz))
     .catch((err) => new Error(err))
